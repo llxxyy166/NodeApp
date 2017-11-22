ac6d0dd58dbcfc4e2bb4ca083314dad262e6515d
Xinye Lei🐶 🐼 🦊 🐯 <xinyelei@amazon.com>
2017-11-22 12:28:02 -0800

    add webpack

---
+++ public/javascripts/login.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $.post( "/users/login", $("form").serialize())
+            .done(function () {
+                window.location.replace("/");
+            })
+            .fail(function () {
+                $("#error-label").attr("hidden", false);
+            });
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 /**
  * Created by xinyelei on 11/12/17.
  */
+const path = require('path');
+
+module.exports = {
+    entry: './public/javascripts/login.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "public")
+        ]
+    }
+};
