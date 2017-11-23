b970947e50c50fc0d3ad21c2e1ff5a814338a204
Xinye Lei🐶 🐼 🦊 🐯 <xinyelei@amazon.com>
2017-11-22 16:03:07 -0800

    12345

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
-import $ from "jquery/dist/jquery.min";
-import "uikit/dist/js/uikit-icons.min";
-import "uikit/dist/js/uikit.min";
 import "uikit/dist/css/uikit.css";
 
+import $ from "jquery/dist/jquery.min";
+import UIkit from "uikit/dist/js/uikit.min";
+import Icons from "uikit/dist/js/uikit-icons.min";
+
+UIkit.use(Icons);
 window.$ = window.jQuery = $;
